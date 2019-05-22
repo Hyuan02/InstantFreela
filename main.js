1ff672d1191cd7fa1d75c4b58b8670bf350335e7
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2019-05-22 09:10:54 -0300

    inicio de web-app para oferecimento de rapidos freelas

---
+++ main.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    var app = new Vue({
+        el:"#app",
+        data: {  
+            titulo: "Aula 01 - Vue JS do jeito ninja!",
+            nome:"",
+            username:"",
+            email:"",
+            telefone:"",
+            pass:"",
+            pass2:"",
+            passCrypt:"",
+            aba:0
+        }
+    });
+}
